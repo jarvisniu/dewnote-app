79b9eccd3658b39e8aec9fe2ae588e6a55c1de4b
Jarvis Niu <jarvisniu@gmail.com>
2019-01-26 02:08:33 +0800

    added macos build support

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const os = require('os')
 
 let mainWindow
@@ -10,11 +10,35 @@ function createWindow () {
     icon: __dirname + os.platform === 'win32' ? '/logo.ico' : '/logo.png'
   })
   mainWindow.loadURL('http://dewnote.com')
-  mainWindow.setMenu(null)
+  if (process.platform !== 'darwin') {
+    mainWindow.setMenu(null)
+  }
 
   mainWindow.on('closed', function () {
     mainWindow = null
   })
+  
+  // Create the Application's main menu TO SUPPORT COPY/PASTE!
+  var template = [{
+      label: "Application",
+      submenu: [
+          { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+          { type: "separator" },
+          { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+      ]}, {
+      label: "Edit",
+      submenu: [
+          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+          { type: "separator" },
+          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 app.on('ready', createWindow)
